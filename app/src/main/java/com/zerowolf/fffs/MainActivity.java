445cc22af2b5f683b5014b4441d349bcff9b222e
ZEROwolf <zerowolfhwang@gmail.com>
2019-11-07 14:00:14 +0800

    initial

--- app/src/main/java/com/zerowolf/fffs/MainActivity.java
+++ app/src/main/java/com/zerowolf/fffs/MainActivity.java
@@ -1,13 +1,33 @@
 package com.zerowolf.fffs;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zerowolf.jsbridge.BridgeWebView;
+import com.zerowolf.jsbridge.DefaultHandler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BridgeWebView mBridgeWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        mBridgeWebView = findViewById(R.id.webViewRelease);
+        initWebView();
+    }
+
+
+    private void initWebView() {
+        mBridgeWebView.setDefaultHandler(new DefaultHandler());
+
+        mBridgeWebView.loadUrl("http://fffs.wxjcy.cn");
+
+//        mBridgeWebView.registerHandler("getNativeTest") { "asda", }
     }
 }
