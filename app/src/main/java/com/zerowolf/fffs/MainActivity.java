dd6895e9008a7f7d4f0c1928b2670c51c8cb7d48
ZEROwolf <zerowolfhwang@gmail.com>
2019-11-07 23:44:06 +0800

    添加 JavaScript 的桥接并实现返回的按钮的打开关闭点击监听

--- app/src/main/java/com/zerowolf/fffs/MainActivity.java
+++ app/src/main/java/com/zerowolf/fffs/MainActivity.java
@@ -1,8 +1,13 @@
 package com.zerowolf.fffs;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.zerowolf.jsbridge.BridgeHandler;
 import com.zerowolf.jsbridge.BridgeWebView;
+import com.zerowolf.jsbridge.CallBackFunction;
 import com.zerowolf.jsbridge.DefaultHandler;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private BridgeWebView mBridgeWebView;
+    private ImageView mBackImage;
+    private Button mTestBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,28 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         mBridgeWebView = findViewById(R.id.webViewRelease);
+        mBackImage = findViewById(R.id.backImage);
+        mTestBtn = findViewById(R.id.testBtn);
+
+        mTestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBackImage.setVisibility(View.GONE);
+            }
+        });
+
+        mBackImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBridgeWebView.callHandler("listenBackIconClick", "原生端点击了返回按钮", new CallBackFunction() {
+                    @Override
+                    public void onCallBack(String data) {
+
+                    }
+                });
+            }
+        });
+
         initWebView();
     }
 
@@ -26,8 +55,34 @@ public class MainActivity extends AppCompatActivity {
     private void initWebView() {
         mBridgeWebView.setDefaultHandler(new DefaultHandler());
 
-        mBridgeWebView.loadUrl("http://fffs.wxjcy.cn");
+        boolean isDev = false;
 
+        if (isDev) {
+            mBridgeWebView.loadUrl("file:///android_asset/demo.html");
+        } else {
+            mBridgeWebView.loadUrl("http://fffs.wxjcy.cn");
+        }
+
+        registerHandler();
 //        mBridgeWebView.registerHandler("getNativeTest") { "asda", }
     }
+
+    private void registerHandler() {
+        mBridgeWebView.registerHandler("openBackIcon", new BridgeHandler() {
+            @Override
+            public void handler(String data, CallBackFunction function) {
+                mBackImage.setVisibility(View.VISIBLE);
+            }
+        });
+
+        mBridgeWebView.registerHandler("closeBackIcon", new BridgeHandler() {
+            @Override
+            public void handler(String data, CallBackFunction function) {
+                mBackImage.setVisibility(View.GONE);
+            }
+        });
+
+    }
+
+
 }
